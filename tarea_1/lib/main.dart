872dd5da13795df68fd2b37e58fa1a8f3a8a5349
valrmzl <valeria.ramirez@iteso.mx>
2023-08-27 23:49:37 -0600

    app 1 funcionando correctamente

--- tarea_1/lib/main.dart
+++ tarea_1/lib/main.dart
@@ -4,122 +4,162 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyApp> createState() => _MyAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyAppState extends State<MyApp> {
+  Color iconoColor1 = Colors.black;
+  Color iconoColor2 = Colors.black;
+  Color iconoColor3 = Colors.black;
+  Color iconoColor4 = Colors.black;
 
-  void _incrementCounter() {
+  void cambiarColor(int icono, BuildContext context, String nombreIcono) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      final snackBar = SnackBar(content: Text('Icono $nombreIcono clickeado'), duration: Duration(seconds: 2));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+      if (icono == 1) {
+        iconoColor1 = (iconoColor1 == Colors.black) ? Colors.indigo : Colors.black;
+      } else if (icono == 2) {
+        iconoColor2 = (iconoColor2 == Colors.black) ? Colors.indigo : Colors.black;
+      } else if (icono == 3) {
+        iconoColor3 = (iconoColor3 == Colors.black) ? Colors.indigo : Colors.black;
+      } else if (icono == 4) {
+        iconoColor4 = (iconoColor4 == Colors.black) ? Colors.indigo : Colors.black;
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Mc Flutter"),
+        ),
+        body: Column(
+          children: [
+            Container(
+              height: 200,
+              width: double.infinity,
+              alignment: Alignment.center,
+              margin: const EdgeInsets.all(20),
+              padding: const EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 3),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: [
+                          Icon(Icons.account_circle, size: 48),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            "Flutter McFlutter",
+                            style: TextStyle(
+                              fontSize: 24,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Experienced App Developer",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          Text("123 Main Street"),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text("(415-555-0198)"),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Builder(
+                        builder: (BuildContext builderContext) {
+                          return GestureDetector(
+                            onTap: () => cambiarColor(1, builderContext, "persona"),
+                            child: Icon(
+                              Icons.accessibility,
+                              size: 32,
+                              color: iconoColor1,
+                            ),
+                          );
+                        },
+                      ),
+                      Builder(
+                        builder: (BuildContext builderContext) {
+                          return GestureDetector(
+                            onTap: () => cambiarColor(2, builderContext, "reloj"),
+                            child: Icon(
+                              Icons.timer,
+                              size: 32,
+                              color: iconoColor2,
+                            ),
+                          );
+                        },
+                      ),
+                      Builder(
+                        builder: (BuildContext builderContext) {
+                          return GestureDetector(
+                            onTap: () => cambiarColor(3, builderContext,"android"),
+                            child: Icon(
+                              Icons.phone_android,
+                              size: 32,
+                              color: iconoColor3,
+                            ),
+                          );
+                        },
+                      ),
+                      Builder(
+                        builder: (BuildContext builderContext) {
+                          return GestureDetector(
+                            onTap: () => cambiarColor(4, builderContext, "iphone"),
+                            child: Icon(
+                              Icons.phone_iphone,
+                              size: 32,
+                              color: iconoColor4,
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
